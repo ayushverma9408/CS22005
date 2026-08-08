dcc5abd78fd5371c1c26cc45951b35fecce16adf
Ayush Verma <ayushverma19341@gmail.com>
2026-08-08 21:26:48 +0530

    Update q1-l2.py

--- Lab 2/q1-l2.py
+++ Lab 2/q1-l2.py
@@ -1,66 +1,83 @@
 '''
-Develop a Python program to analyse student course enrolments by using set operations (union, intersection, difference) to identify students enrolled in common and unique courses, and tuples to store student details.  
+Develop a Python program to analyse student course enrolments by using set operations (union, intersection, difference) to identify students enrolled in common and unique courses, and tuples to store student details.
 '''
+
 students = []
 course_sets = []
-
-n = int(input("Enter number of students: "))
  
-for i in range(n):
-    print(f"\nEnter details of Student {i+1}")
+def input_students():
+    n = int(input("Enter number of students: "))
 
-    roll = input("Enter Roll No: ")
-    name = input("Enter Name: ")
+    for i in range(n):
+        print(f"\nEnter details of Student {i + 1}")
 
-    students.append((roll, name))    
-
-    m = int(input("Enter number of courses: "))
-    courses = set()
-
-    for j in range(m):
-        course = input(f"Enter course {j+1}: ")
-        courses.add(course)
-
-    course_sets.append(courses)
+        roll = input("Enter Roll No: ")
+        name = input("Enter Name: ")
  
-print("\nStudent Details")
-for i in range(n):
-    print(f"\nStudent {i+1}: {students[i]}")
-    print("Courses:", course_sets[i])
- 
-all_courses = set()
-for s in course_sets:
-    all_courses = all_courses.union(s)
+        students.append((roll, name))
 
-print(f"\nAll Courses (Union): {all_courses}") 
- 
-common = course_sets[0]
-for i in range(1, n):
-    common = common.intersection(course_sets[i])
+        m = int(input("Enter number of courses: "))
+        courses = set()
 
-if common:
-    print(f"\nCommon Courses (Intersection): {common}")
-if not common:
-    print("No common courses among all students.")
- 
-print("\nPairwise Common Courses:")
-for i in range(n):
-    for j in range(i + 1, n):
-        common_pair = course_sets[i].intersection(course_sets[j])
-        if common_pair:
-            print(f"{students[i][1]} and {students[j][1]}: {common_pair}")
-        else:
-            print(f"{students[i][1]} and {students[j][1]}: No common courses")
- 
-print("\nUnique Courses:")
-for i in range(n):
-    unique = course_sets[i]
+        for j in range(m):
+            course = input(f"Enter course {j + 1}: ")
+            courses.add(course)
 
-    for j in range(n):
-        if i != j:
-            unique = unique.difference(course_sets[j])
+        course_sets.append(courses)
+ 
+def display_students():
+    print("\nStudent Details")
 
-    if unique:
-        print(f"{students[i][1]}: {unique}")
+    for i in range(len(students)):
+        print(f"\nStudent {i + 1}: {students[i]}")
+        print("Courses:", course_sets[i])
+ 
+def find_union():
+    all_courses = set()
+
+    for courses in course_sets:
+        all_courses = all_courses.union(courses)
+
+    print(f"\nAll Courses (Union): {all_courses}")
+ 
+def find_common():
+    common = course_sets[0]
+
+    for i in range(1, len(course_sets)):
+        common = common.intersection(course_sets[i])
+
+    if common:
+        print(f"\nCommon Courses among all students: {common}")
     else:
-        print(f"{students[i][1]}: No unique courses")
+        print("\nNo common courses among all students.")
+ 
+    print("\nPairwise Common Courses:") 
+    for i in range(len(students)):
+        for j in range(i + 1, len(students)):
+            common_pair = course_sets[i].intersection(course_sets[j])
+
+            if common_pair:
+                print(f"{students[i][1]} and {students[j][1]}: {common_pair}")
+            else:
+                print(f"{students[i][1]} and {students[j][1]}: No common courses")
+ 
+def find_unique():
+    print("\nUnique Courses:")
+
+    for i in range(len(students)):
+        unique = course_sets[i]
+
+        for j in range(len(students)):
+            if i != j:
+                unique = unique.difference(course_sets[j])
+
+        if unique:
+            print(f"{students[i][1]}: {unique}")
+        else:
+            print(f"{students[i][1]}: No unique courses") 
+ 
+input_students()
+display_students()
+find_union()
+find_common()
+find_unique()
